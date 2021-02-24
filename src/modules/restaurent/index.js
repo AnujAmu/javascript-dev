f0a8f15a171de8619f72af9f0bdadaa63fa97182
anujkumar97 <anuj@leewayhertz.com>
2021-02-24 15:53:09 +0530

    formatting of index.js file.

--- src/modules/restaurent/index.js
+++ src/modules/restaurent/index.js
@@ -3,6 +3,7 @@ import BaseComponent from '../baseComponent'
 import RestaurentComponent from './restaurentComponent'
 import data from './restaurentData.json'
 import moment from 'moment'
+
 class Restaurent extends BaseComponent {
     constructor(props) {
         super(props);
@@ -14,10 +15,22 @@ class Restaurent extends BaseComponent {
     }
 
     async componentDidMount() {
-        let date = new Date();
-        console.log(date)
         await this.getRestaurentData();
         this.parseData(this.state.startTime);
+    }
+
+    getRestaurentData = async () => {
+        await fetch("https://run.mocky.io/v3/b0f3e975-b815-4e88-8a6a-84af59fe32eb", { method: "GET" })
+            .then((response) => {
+                return response.json()
+            })
+            .then((responseData) => {
+                let data = this.parseResData(responseData)
+                this.setState({ restaurentArray: data, itemArray: data });
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
 
     parseResData = (request) => {
@@ -27,23 +40,15 @@ class Restaurent extends BaseComponent {
                 "Kushi Tsuru": request[index]["Kushi Tsuru"],
                 "Mon-Sun 11:30 am - 9 pm": request[index]["Mon-Sun 11:30 am - 9 pm"],
                 "isClosed": "Closed"
-
             })
         }
         return parsedData;
     }
-    applyFilter = (value, name) => {
-        this.setState({ [name]: value })
-        this.parseData(value)
-    }
+
     parseData = (timestamp) => {
         let day = moment(timestamp).format("ddd").toLowerCase();
         let time = moment(timestamp).format("HH:mm A");
-        console.log("day is", day)
-        console.log("time is", time)
-
         let filteredRestaurents = data.filter((item) => {
-            // console.log("Item is",item.name, item.day[day].startTime.length, item.day[day].endTime.length)
             if (item.day[day])
                 return (item.day[day].startTime <= time && time <= item.day[day].endTime)
         })
@@ -54,20 +59,16 @@ class Restaurent extends BaseComponent {
                 this.state.itemArray[index].isClosed = "Closed"
             })
         } else {
-            let diffArray= this.state.itemArray.filter(item1 =>
-                !filteredRestaurents.some(item2 =>item2.name===item1['Kushi Tsuru']))
+            let diffArray = this.state.itemArray.filter(item1 =>
+                !filteredRestaurents.some(item2 => item2.name === item1['Kushi Tsuru']))
 
-                console.log("diffArray is", diffArray)
-                this.state.itemArray.map((item, index) => {
-                    diffArray.map((restaurent) => {
-                        if (item['Kushi Tsuru'] === restaurent['Kushi Tsuru']) {
-                            this.state.itemArray[index].isClosed = "Closed"
-                        }
-                    })
+            this.state.itemArray.map((item, index) => {
+                diffArray.map((restaurent) => {
+                    if (item['Kushi Tsuru'] === restaurent['Kushi Tsuru']) {
+                        this.state.itemArray[index].isClosed = "Closed"
+                    }
                 })
-            // this.setState({restaurentArray: })
-            // console.log("filteredRestaurents are", filteredRestaurents)
-            let myArray = []
+            })
             this.state.itemArray.map((item, index) => {
                 filteredRestaurents.map((restaurent) => {
                     if (item['Kushi Tsuru'] === restaurent.name) {
@@ -78,20 +79,7 @@ class Restaurent extends BaseComponent {
         }
         this.setState({ restaurentArray: this.state.itemArray })
     }
-    getRestaurentData = async () => {
-        await fetch("https://run.mocky.io/v3/b0f3e975-b815-4e88-8a6a-84af59fe32eb", { method: "GET" })
-            .then((response) => {
-                return response.json()
-            })
-            .then((responseData) => {
-                let data = this.parseResData(responseData)
-                console.log(data)
-                this.setState({ restaurentArray: data, itemArray: data });
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    }
+
     onChangeEvent = (event) => {
         let value = event.target.value.toLowerCase();
         if (value && value.length) {
@@ -107,6 +95,11 @@ class Restaurent extends BaseComponent {
             this.setState({ restaurentArray: this.state.itemArray })
         }
     };
+
+    applyFilter = (value, name) => {
+        this.setState({ [name]: value })
+        this.parseData(value)
+    }
 
     render() {
         return (
